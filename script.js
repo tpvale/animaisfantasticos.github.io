e3d7bd49f5c6d3d3f23ffce699d988e329f3b459
tpvale <tpv.tiago@gmail.com>
2022-04-20 18:05:54 -0300

    Adicionando novas funções

---
+++ script.js
@@ -0,0 +1,34 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+      });
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+const accordionList = document.querySelectorAll('.js-accordion dt');
+
+function activeAccordion() {
+  this.classList.toggle('active');
+  this.nextElementSibling.classList.toggle('active');
+}
+
+accordionList.forEach((item) => {
+  item.addEventListener('click', activeAccordion); 
+})
